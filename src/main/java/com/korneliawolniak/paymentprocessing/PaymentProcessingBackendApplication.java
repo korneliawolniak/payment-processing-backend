49fa888f8e18e9b96442d4d254ab23a1966d92ba
korneliawolniak <korneliawolniak@gmail.com>
2026-08-12 19:36:18 +0200

    Initial Spring Boot setup

---
+++ src/main/java/com/korneliawolniak/paymentprocessing/PaymentProcessingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.korneliawolniak.paymentprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentProcessingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentProcessingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/korneliawolniak/paymentprocessing/PaymentProcessingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korneliawolniak.paymentprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentProcessingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
