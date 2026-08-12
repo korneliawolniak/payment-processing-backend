877fd8a615f08b7caf5239170caa83fed0ee670e
korneliawolniak <korneliawolniak@gmail.com>
2026-08-12 21:44:07 +0200

    Configure Spotless

--- src/main/java/com/korneliawolniak/paymentprocessing/PaymentProcessingBackendApplication.java
+++ src/main/java/com/korneliawolniak/paymentprocessing/PaymentProcessingBackendApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PaymentProcessingBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PaymentProcessingBackendApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(PaymentProcessingBackendApplication.class, args);
+  }
 }
